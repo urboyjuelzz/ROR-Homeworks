c78c40c4b0dfbeccf7d0c6b16ce4cf5d35f291de
Joel Ceballos <urboyjuelzz@gmail.com>
2017-01-26 17:04:51 -0500

    completed homework 2

--- Homework-2/problem-1.rb
+++ Homework-2/problem-1.rb
@@ -1,11 +1,13 @@
 # Add a comment at the beginning of the following code describing in plain English what it does in general, then add a comment before each line explaining what that particular line does specifically.
 
 
-# We are defining a method called fizzbuzz w/ perameter number
+# This code is looping from 1 up to limit… when a number in that range is divisible and has a remainder of 0 for the case statement > print the the then result > or else print the number….
+
+# We are defining a method called fizzbuzz w/ parameter number
 def fizzbuzz(number)
   # Each Case occurs when the condition is true
   case
-    # modulo operater that says when Number is / by 15,3 or 5 has a remainder of 0, enter the string respectively.
+    # modulo operater that says when Number is / by 15,3 or 5 and has a remainder of 0, then enter the string respectively.
     when number % 15 == 0 then 'Fizzbuzz'
     when number % 3  == 0 then 'Fizz'
     when number % 5  == 0 then 'Buzz'
@@ -16,7 +18,7 @@ def fizzbuzz(number)
 # ends the method
 end
 
-# We are defining method called fizzbuzz_to w/ limit perameter.
+# We are defining method called fizzbuzz_to w/ limit parameter.
 def fizzbuzz_to(limit)
   # a loop that starts at 1 calling the method upto what ever limit is entered, then it will do whats in the block.
   1.upto(limit) do |i|
@@ -26,3 +28,5 @@ def fizzbuzz_to(limit)
   end
 # ends the method
 end
+
+fizzbuzz_to.(16)
--- Homework-2/problem-2.rb
+++ Homework-2/problem-2.rb
@@ -9,22 +9,41 @@
 # Create a new instance of the Homework class and call each of the previous methods.
 
 class Homework
+  # use a method to print string in upcase.
   def shout (word)
-    puts '#{word}'.upcase
+    return "#{word}".upcase
+  end
+  #use methods thats takes 2 arguments to repeat a string based on the number assigned to one of the arguments.
+  def repeat_string(write_word, number_of_times)
+    1.upto(number_of_times) do |i|
+      puts ("#{write_word}")
+    end
   end
 
-  # def repeat_string(write_word, number_of_times=3)
-  #   1.upto(number_of_times) do |number_of_times|
-  #   puts ("#{write_word}")
-  # end
+# use a method that takes 2 arguments that converts strings in 2 array into a hash(key: value).
+  def calendar(holidays, dates)
+    result = {}
 
-  # def calendar (["MLK", "Columbus"], ["Jan 16", "Oct 9"])
-  #   return [0] + [0] && [1] + [1]
-  # end
+    0.upto(holidays.length - 1) do |i|
+      result[holidays[i]] = dates[i]
+    end
+
+    result
+  end
 end
 
-shout ('Quieeeet!')
-# repeat_string ("love", 3)
-# calendar ()
 
-# problems = Homework.new
+instance_of_homwork = Homework.new
+
+(Homework.new.shout('Quieeeet!'))
+
+Homework.new.repeat_string('love', 3)
+
+puts(Homework.new.calendar(['MLK', 'Columbus', 'Halloween'], ['Jan 16', 'Oct 9', 'Oct 31']))
+
+
+
+# notes for me to understan Hash
+# my_hash = {'name' => 'cody'} how you assign a hash as a value
+# my_hash['Name'] = call a hash key
+# my_hash['name'] = 'Joel'
