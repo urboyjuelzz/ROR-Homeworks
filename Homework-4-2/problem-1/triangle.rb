a3fe86326e57d7eff4632bc8d06f17d62f227abc
Joel Ceballos <urboyjuelzz@gmail.com>
2017-02-06 14:55:35 -0500

    Initial HW4 commit

---
+++ Homework-4-2/problem-1/triangle.rb
@@ -0,0 +1,16 @@
+# Problem 1
+# Download the file homework-4-problem-1.zip from Canvas and unzip it to use as the template for this problem.
+# Create a class in triangle.rb called Triangle.
+# Run the code in tests.rb. This file contains a series of tests that will verify the correctness of your Triangle class by calling the kind method for triangles with sides of various lengths and checking the value returned. When you run the tests, you will see a report of which tests failed. You haven’t written any code for Triangle yet, so all the tests will fail initially—your job is to make them all pass!
+# Add a constructor to Triangle that takes three numbers as parameters representing the lengths of the sides of the triangle.
+# Add a method to Triangle called kind that returns a symbol indicating whether the triangle is equilateral, isosceles, or scalene. The method should raise an exception for triangles with any side of length less than or equal to zero and for triangles whose sides violate the triangle inequality (Links to an external site.).
+
+class Triangle
+
+  def my_triangle_length (side1, side2, side3)
+  end
+
+  def kind()
+  end
+
+end
